51630bd530816762c3e00eba2c20236a5e90ab96
Nikolay-Ivanchenko94 <ivanchenkon94@gmail.com>
2024-03-13 11:36:30 +0300

    1.Add Empty WeekWeatherFragment. 2.Add Empty MapFragment.

---
+++ app/src/main/java/com/example/wethernow/fragment/WeekWeatherFragment.java
@@ -0,0 +1,21 @@
+package com.example.wethernow.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wethernow.R;
+
+
+public class WeekWeatherFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_week_weather, container, false);
+    }
+}
