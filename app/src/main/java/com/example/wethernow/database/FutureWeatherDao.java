d15ba5baf10d253e590fa95544927a2f80325123
Nikolay-Ivanchenko94 <ivanchenkon94@gmail.com>
2024-04-08 12:27:45 +0300

    !

--- app/src/main/java/com/example/wethernow/database/FutureWeatherDao.java
+++ app/src/main/java/com/example/wethernow/database/FutureWeatherDao.java
@@ -1,4 +1,22 @@
 package com.example.wethernow.database;
 
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.wethernow.models.modelsforecast.FutureForecast;
+
+import java.util.List;
+
+@Dao
 public interface FutureWeatherDao {
+
+    @Insert
+    public void insertFutureWeather(FutureForecast futureForecast);
+
+    @Query("SELECT * FROM FutureForecast")
+    public List<FutureForecast> getFutureForecast();
+
+
 }
--- app/src/main/java/com/example/wethernow/database/MyDataBase.java
+++ app/src/main/java/com/example/wethernow/database/MyDataBase.java
@@ -1,4 +1,12 @@
 package com.example.wethernow.database;
 
-public class MyDataBase {
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.wethernow.models.modelsforecast.FutureForecast;
+
+@Database(entities = {FutureForecast.class}, version = 1)
+public abstract  class MyDataBase extends RoomDatabase {
+    public abstract FutureWeatherDao futureWeatherDao();
 }
