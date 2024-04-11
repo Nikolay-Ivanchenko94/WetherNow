e7f3ba0c17c2950ddb12535404e2c00d168338b0
Nikolay-Ivanchenko94 <ivanchenkon94@gmail.com>
2024-04-11 12:37:47 +0300

    !

--- app/src/main/java/com/example/wethernow/database/HourDao.java
+++ app/src/main/java/com/example/wethernow/database/HourDao.java
@@ -1,11 +1,13 @@
 package com.example.wethernow.database;
 
+import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
 
-public interface HourDao {
+@Dao
+   public interface HourDao {
 
     @Insert
     public void insertHourDBList(List<HourDB> hourDB);
