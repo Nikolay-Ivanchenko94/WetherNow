ee40161501e0bd2237e0fd5f37317af832940a9c
Nikolay-Ivanchenko94 <ivanchenkon94@gmail.com>
2024-04-25 14:48:11 +0300

    !

--- app/src/main/java/com/example/wethernow/fragment/CityWeatherFragment.java
+++ app/src/main/java/com/example/wethernow/fragment/CityWeatherFragment.java
@@ -81,6 +81,8 @@ public class CityWeatherFragment extends Fragment {
             public void onResponse(retrofit2.Call<FutureForecast> call, retrofit2.Response<FutureForecast> response) {
                 if (response.isSuccessful() && response.body() != null){
                     FutureForecast futureForecast = response.body();
+                    ArrayList<Hour> hours = new ArrayList<>(futureForecast.getForecast().getForecastday().get(0).getHour());
+
 
 
                     MyDataBase db = Room.databaseBuilder(getActivity().getApplicationContext(),
